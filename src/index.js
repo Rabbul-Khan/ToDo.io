5a5f5591dc52651241f00b9e43e3f3253ed9772d
Rabbul-Khan <mohammed.rabbul.hossain.khan@g.bracu.ac.bd>
2023-01-12 22:12:11 +0600

    Set up the development environment using npm and webpack

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { add } from "./add.js";
+import "./main.css";
+
+alert(add(5, 8));
---
+++ webpack.common.js
@@ -0,0 +1,22 @@
+const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+    vendor: "./src/vendor.js",
+  },
+  output: {
+    filename: "main.[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
