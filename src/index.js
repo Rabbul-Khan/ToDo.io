8a357a5daf17175a0d9e3e17a0fe31bb328725b4
Rabbul-Khan <mohammed.rabbul.hossain.khan@g.bracu.ac.bd>
2023-01-19 15:35:10 +0600

    Improved and cleaned the design

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./main.css";
 import { addTaskButton } from "./addTaskButton";
 import { submit } from "./submitButton";
+import { setTodayDate } from "./setTodayDate";
 
+setTodayDate();
 addTaskButton();
 submit();
